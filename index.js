72b89b0bdfb2b9e0e5ff57eb23a69fff19248df9
Brent Lewis <brent.lewis08@gmail.com>
2018-05-22 22:22:58 -0500

    Add list of American birds, includes common name and scientific name

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-const rookery = {
+const americanBirds = require('./birds/american-birds');
 
+const getByCommonName = name => {
+    return americanBirds
+        .filter(b => b.commonName === name)[0];
+};
+
+const getByScientificName = name => {
+    return americanBirds
+        .filter(b => b.scientificName === name)[0];
+};
+
+const rookery = {
+    getByCommonName,
+    getByScientificName
 };
 
 module.exports = rookery;
